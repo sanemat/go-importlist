d3d5ce03be3a4e0cf88e073fd6a671747dca7aa6
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-01-05 02:24:38 +0700

    chore(test): rename stub file
    fix wrong dependencies

--- cli_test.go
+++ cli_test.go
@@ -9,7 +9,7 @@ import (
 func TestImportlist_importList(t *testing.T) {
 	t.Run("single line", func(t *testing.T) {
 		expected := []string{"github.com/sanemat/go-xgoinstall/cmd/x-go-install"}
-		data, err := ioutil.ReadFile("test/tools1.go")
+		data, err := ioutil.ReadFile("test/tools1.go.txt")
 		if err != nil {
 			t.Errorf("error should be nil, but: %s", err)
 		}
@@ -24,7 +24,7 @@ func TestImportlist_importList(t *testing.T) {
 
 	t.Run("multi lines", func(t *testing.T) {
 		expected := []string{"github.com/sanemat/go-xgoinstall/cmd/x-go-install", "golang.org/x/lint/golint"}
-		data, err := ioutil.ReadFile("test/tools2.go")
+		data, err := ioutil.ReadFile("test/tools2.go.txt")
 		if err != nil {
 			t.Errorf("error should be nil, but: %s", err)
 		}
